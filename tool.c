2f8136816eda409c0bf605a2d9f94dd7ccfd5b9a
Changho <ho9983@naver.com>
2020-11-16 07:26:46 -0800

    lab3TeamProject

---
+++ tool.c
@@ -0,0 +1,7 @@
+README.md
+test
+test.c
+tool2.c
+tool.c
+try
+try.c
---
+++ try.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(void){
+printf("hi\nhi\nhi\nhi\nhi\nhi\nhi\nhi\n");
+
+sleep(10);
+}
